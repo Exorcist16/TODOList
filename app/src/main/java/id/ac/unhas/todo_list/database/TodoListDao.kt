9711c81ae607c71fa2597f64bffa0f78ac22f70d
Achmad Asjar <millangaab17d@student.unhas.ac.id>
2020-05-29 03:51:33 +0800

    first commit

---
+++ app/src/main/java/id/ac/unhas/todo_list/database/TodoListDao.kt
@@ -0,0 +1,20 @@
+package id.ac.unhas.todo_list.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import id.ac.unhas.todo_list.todo.TodoList
+
+@Dao
+interface TodoListDao {
+    @Query("SELECT * from todoList ")
+    fun getTodoList(): LiveData<List<TodoList>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTodoList(todoList: TodoList)
+
+    @Delete
+    suspend fun deleteTodoList(todoList: TodoList)
+
+    @Update
+    suspend fun updateTodoList(todoList: TodoList)
+}
